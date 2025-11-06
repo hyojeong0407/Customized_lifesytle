231bb3255147a3893b4b92e4ca18ec9aaac0743a
hyojeong0407 <hyogomushroom562@gmail.com>
2025-11-06 21:42:38 +0900

    사용자 맞춤 피드백 화면 구현

--- customize/src/App.js
+++ customize/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import deepStreamImage from './Deep Stream.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="image">
+        <img className="deep-stream" src={deepStreamImage} alt="Deep stream" />
+      </div>
+
+      <button className="button-data">
+        <div className="text-wrapper">
+          저장된 데이터
+          <br />
+          확인
+        </div>
+      </button>
+
+      <button className="button-feedback">
+        <div className="text-wrapper">
+          저장된 데이터
+          <br />
+          기반 피드백
+        </div>
+      </button>
+    </>
   );
 }
 
