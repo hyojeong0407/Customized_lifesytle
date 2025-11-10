a85924937cc80137c6647dc1f593e20b411e05cf
hyojeong0407 <hyogomushroom562@gmail.com>
2025-11-10 21:17:37 +0900

    사용자 맞춤 피드백 화면 코드 수정

--- customize/src/App.js
+++ customize/src/App.js
@@ -1,14 +1,32 @@
+import { useState } from 'react';
 import './App.css';
-import deepStreamImage from './Deep Stream.png';
+import deepStreamImage from './Deep_Stream.png';
+import CheckData from './components/CheckData';
+import Checkfig from './components/Checkfig';
 
 function App() {
+  const [showCheckData, setShowCheckData] = useState(false);
+  const [showCheckfig, setShowCheckfig] = useState(false);
+
+  if (showCheckData) {
+    return <CheckData onClose={() => setShowCheckData(false)} />;
+  }
+
+  if (showCheckfig) {
+    return <Checkfig onClose={() => setShowCheckfig(false)} />;
+  }
+
   return (
     <>
       <div className="image">
-        <img className="deep-stream" src={deepStreamImage} alt="Deep stream" />
+        <img
+          className="deep-stream"
+          src={deepStreamImage}
+          alt="Deep stream"
+        />
       </div>
 
-      <button className="button-data">
+      <button className="button-data" onClick={() => setShowCheckData(true)}>
         <div className="text-wrapper">
           저장된 데이터
           <br />
@@ -16,7 +34,7 @@ function App() {
         </div>
       </button>
 
-      <button className="button-feedback">
+      <button className="button-feedback" onClick={() => setShowCheckfig(true)}>
         <div className="text-wrapper">
           저장된 데이터
           <br />
