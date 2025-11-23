f4e592ac087ae084df8c4f2cee58c8fc8d15338d
hyojeong0407 <hyogomushroom562@gmail.com>
2025-11-23 16:36:19 +0900

    약 정보 화면 구현

--- customize/src/components/MediInfo.js
+++ customize/src/components/MediInfo.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import './MediInfo.css';
+import deepStreamImage from '../Deep_Stream.png';
 
-const MediInfo = () => {
+const MediInfo = ({ onClose }) => {
     return (
-        <div>
-            
+        <div className='mediinfo-container'>
+            <div className="image">
+                <img
+                    className="deep-stream"
+                    src={deepStreamImage}
+                    alt="Deep stream"
+                    onClick={() => onClose()}
+                />
+            </div>
+
+            <div className='mediinfo-text'>
+                약의 정보가 없습니다.
+            </div>
         </div>
     );
 };
