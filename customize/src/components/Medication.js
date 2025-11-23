8f82fb7a67a43cf89cee09bc591efbb9777f98fa
hyojeong0407 <hyogomushroom562@gmail.com>
2025-11-23 16:35:59 +0900

    복약 관리 화면 구현

--- customize/src/components/Medication.js
+++ customize/src/components/Medication.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import './Medication.css';
+import deepStreamImage from '../Deep_Stream.png';
 
-const Medication = () => {
+const Medication = ({ onOpenMediInfo,onClose }) => {
+
     return (
-        <div>
-            
+        <div className='medication-container'>
+            <div className="image">
+                <img
+                    className="deep-stream"
+                    src={deepStreamImage}
+                    alt="Deep stream"
+                    onClick={() => onClose()}
+                />
+            </div>
+
+            <button className='mediinfo' onClick={() => typeof onOpenMediInfo === 'function' && onOpenMediInfo()}>
+                복용 약 정보 보기
+            </button>
+
+            <button className='takepill'>
+                알약 찍기
+            </button>
+
+            <button className='upload-picture'>
+                사진 업로드
+            </button>
         </div>
     );
 };
